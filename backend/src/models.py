580a9db990d93256480ac179cdaaeb76bab43882
Italiz <italizv@gmail.com>
2023-10-23 18:19:11 -0400

    Completed setting up models

--- backend/src/models.py
+++ backend/src/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 from db import Base
 
 
@@ -9,6 +10,8 @@ class Deck(Base):
     deck_name: Column(String)
     datetime_created: Column(DateTime)
 
+    cards = relationship("Card", back_populates="deck")
+
 
 class Card(Base):
     __tablename__ = "cards"
@@ -16,3 +19,12 @@ class Card(Base):
     id = Column(Integer, primary_key=True)
     question = Column(String)
     answer = Column(String)
+
+    deck = relationship("Deck", back_populates="cards")
+
+
+class CardToDeck(Base):
+    __tablename__ = "cards_to_decks"
+
+    card_id = Column(Integer, ForeignKey("card.id"))
+    deck_id = Column(Integer, ForeignKey("deck.id"))
