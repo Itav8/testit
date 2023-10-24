5bff44a128de59d53a1666c4bd962f9629947713
Italiz <italizv@gmail.com>
2023-10-24 18:09:58 -0400

    added a file

--- frontend/src/pages/Decks/Decks.tsx
+++ frontend/src/pages/Decks/Decks.tsx
@@ -1,11 +1,37 @@
-import "./Decks.css";
+import { useEffect, useState } from "react";
+
+export interface Deck {
+  deckId?: number;
+  deckName: string;
+  datetimeCreated?: string;
+}
 
 export const Decks = () => {
+  const [decks, seetDecks] = useState<Deck[]>([]);
+
+  // useEffect(() => {
+  const fetchDecks = async () => {
+    const getDecksUrl = `${import.meta.env.VITE_API_URL}/decks`;
+
+    try {
+      const decksResponse = await fetch(getDecksUrl);
+
+      if (decksResponse.ok) {
+        const decksData = await decksResponse.json();
+        console.log("INSE", decksData);
+        seetDecks(decksData);
+      }
+    } catch (e) {
+      console.log("Error getting list of decks", e);
+    }
+  };
+  fetchDecks();
+  // }, []);
+
+  console.log("DECJS", decks);
   return (
-    <>
-      <div>
-        <h1 className="title">My Decks</h1>
-      </div>
-    </>
+    <div>
+      <h1 className="title">My Decks</h1>
+    </div>
   );
 };
