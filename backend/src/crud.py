7e1f628ffd578f1c0cce199cc924fc47b442effd
Italiz <italizv@gmail.com>
2023-10-24 17:12:00 -0400

    Completed CRUD and routers for decks

--- backend/src/crud.py
+++ backend/src/crud.py
@@ -4,7 +4,6 @@ import schemas
 
 
 def create_deck(db: Session, deck_name: schemas.DeckBase):
-    print("ffsfd", deck_name)
     try:
         print("INSIDE", deck_name)
         new_deck = models.Deck(**deck_name.model_dump())
@@ -16,39 +15,39 @@ def create_deck(db: Session, deck_name: schemas.DeckBase):
         return {"Error": f"{e}"}
 
 
-# def get_list_of_decks(db: Session):
-#     try:
-#         decks = db.query(models.Deck)
+def get_list_of_decks(db: Session):
+    try:
+        decks = db.query(models.Deck)
 
-#         if decks:
-#             return decks
-#         return {"Message": "Deck list is empty"}
-#     except Exception as e:
-#         return {"Error": f"{e}"}
-
-
-# def update_deck(db: Session, deck_id: int, deck: schemas.DeckBase):
-#     try:
-#         current_deck: models.Deck = db.query(models.Deck).get(deck_id)
-
-#         if current_deck:
-#             current_deck.deck_name = deck.deck_name
-#             db.commit()
-#             db.refresh(current_deck)
-#             return current_deck
-#         return {"Message": "Deck not found"}
-#     except Exception as e:
-#         return {"Error": f"{e}"}
+        if decks:
+            return decks
+        return {"Message": "Deck list is empty"}
+    except Exception as e:
+        return {"Error": f"{e}"}
 
 
-# def delete_deck(db: Session, deck_id: int):
-#     try:
-#         current_deck = db.query(models.Deck).get(deck_id)
+def update_deck(db: Session, deck_id: int, deck: schemas.DeckBase):
+    try:
+        current_deck: models.Deck = db.query(models.Deck).get(deck_id)
 
-#         if current_deck:
-#             db.delete(current_deck)
-#             db.commit()
-#             return {"Message": "Deck deleted successfully"}
-#         return {"Message": "Deck not found"}
-#     except Exception as e:
-#         return {"Error": f"{e}"}
+        if current_deck:
+            current_deck.deck_name = deck.deck_name
+            db.commit()
+            db.refresh(current_deck)
+            return current_deck
+        return {"Message": "Deck not found"}
+    except Exception as e:
+        return {"Error": f"{e}"}
+
+
+def delete_deck(db: Session, deck_id: int):
+    try:
+        current_deck = db.query(models.Deck).get(deck_id)
+
+        if current_deck:
+            db.delete(current_deck)
+            db.commit()
+            return {"Message": "Deck deleted successfully"}
+        return {"Message": "Deck not found"}
+    except Exception as e:
+        return {"Error": f"{e}"}
--- backend/src/routers.py
+++ backend/src/routers.py
@@ -8,11 +8,37 @@ import crud
 router = APIRouter()
 
 
-@router.post("/deck/create", response_model=schemas.Deck)
-def create_deck(deck_name: schemas.DeckBase, db: Session = Depends(get_db)):
-    print("ROUTER", deck_name)
+@router.post("/decks", response_model=schemas.Deck)
+def created_deck(deck_name: schemas.DeckBase, db: Session = Depends(get_db)):
     new_deck = crud.create_deck(db, deck_name)
 
     if new_deck is None:
         return HTTPException(status_code=404, detail="Unable to create deck")
     return new_deck
+
+
+@router.get("/decks", response_model=list[schemas.Deck])
+def get_decks(db: Session = Depends(get_db)):
+    decks = crud.get_list_of_decks(db)
+
+    if decks is None:
+        return HTTPException(status_code=404, detail="Unable to find decks")
+    return decks
+
+
+@router.put("/decks/{deck_id}", response_model=schemas.Deck)
+def updated_deck(deck_id: int, deck: schemas.DeckBase, db: Session = Depends(get_db)):
+    updated_deck = crud.update_deck(db, deck_id, deck)
+
+    if updated_deck is None:
+        return HTTPException(status_code=404, detail="Unable to find deck")
+    return updated_deck
+
+
+@router.delete("/decks/{deck_id}", response_model={})
+def deleted_deck(deck_id: int, db: Session = Depends(get_db)):
+    deleted_deck = crud.delete_deck(deck_id, db)
+
+    if deleted_deck is None:
+        return HTTPException(status_code=404, detail="Unable to find deck")
+    return {"Deck deleted Successfully"}
