ca0fef87171f5506973305271fa225af977acf82
Italiz <italizv@gmail.com>
2023-10-26 12:43:18 -0400

    fectch deck for test page

--- frontend/src/pages/Test/Test.tsx
+++ frontend/src/pages/Test/Test.tsx
@@ -1,10 +1,41 @@
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+
+export interface Cards {
+  question: string;
+  answer: string;
+}
 
 export const Test = () => {
   const location = useLocation();
   const pathName = location.pathname.split("/");
   const deckId = pathName[pathName.length - 1];
-  console.log("deckId", deckId)
+  const [cards, setCards] = useState<Cards[]>([])
+
+  const fetchDeck = async (deckId: number) => {
+    const getDeckUrl = `${import.meta.env.VITE_API_URL}/decks/${deckId}`;
+
+    try {
+      const deckResponse = await fetch(getDeckUrl);
+
+      if (deckResponse.ok) {
+        const deckData = await deckResponse.json();
+        const cards = deckData.cards.map((card: Cards) => {
+          return [card.question, card.answer]
+        })
+        setCards(cards);
+      }
+    } catch (error) {
+      console.log("Error getting cards", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchDeck(Number(deckId));
+  }, [deckId]);
+
+  console.log(cards)
+
   return (
     <>
       <div>
